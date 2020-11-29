0202a71a8f001d111568e27ac26a06c0ec4f7940
Navid <navid.nazifi@gmail.com>
2020-11-30 00:27:23 +0100

    1003 JUnitTest #3
    change PasswordUtilsTest

--- src/test/java/at/campus/ads/common/utils/PasswordUtilsTest.java
+++ src/test/java/at/campus/ads/common/utils/PasswordUtilsTest.java
@@ -1,7 +1,6 @@
 package at.campus.ads.common.utils;
 
 
-import at.campus.ads.common.utils.PasswordUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,7 +17,7 @@ class PasswordUtilsTest {
     }
 
     @Test
-    void generateSecurePassword() {
+    void GIVEN_password_WHEN_generateSecurePassword_THEN_returnHashCode() {
         try {
             String password  = "123";
             String generatedPassword  = PasswordUtils.generateSecurePassword(password);
@@ -29,12 +28,24 @@ class PasswordUtilsTest {
     }
 
     @Test
-    void verifyUserPassword() {
+    void GIVEN_passwordAndHash_WHEN_verifyUserPassword_THEN_returnTrue() {
         try {
             String password  = "123";
             String securePassword = "Gcf70h4aWQ1T9NMxE03XM3nq3nCmFGihnO4xMzHMgP0=";
             boolean verified  = PasswordUtils.verifyUserPassword(password, securePassword);
             assertTrue(verified);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void GIVEN_wrongPassword_WHEN_verifyUserPassword_THEN_returnFalse() {
+        try {
+            String password  = "123";
+            String securePassword = "WRONG_PASSWORD";
+            boolean verified  = PasswordUtils.verifyUserPassword(password, securePassword);
+            assertFalse(verified);
         } catch (Exception e) {
             e.printStackTrace();
         }
