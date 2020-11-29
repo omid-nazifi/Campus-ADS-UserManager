6d98c98513ed48f2c4cbdfcea0b81781be51a031
Navid <navid.nazifi@gmail.com>
2020-11-30 00:43:22 +0100

    1003 JUnitTest #3
    rename RegisterService test

--- src/test/java/at/campus/ads/service/RegisterServiceIT.java
+++ src/test/java/at/campus/ads/service/RegisterServiceIT.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RegisterServiceTest {
+public class RegisterServiceIT {
 
     private static final String USERNAME = "user";
     private static final String FIRST_NAME = "test";
