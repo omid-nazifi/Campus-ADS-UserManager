502232c8e39af0e88da57e5838222ed5e1b62542
Navid <navid.nazifi@gmail.com>
2020-11-30 00:44:45 +0100

    1003 JUnitTest #3
    clean code

--- src/test/java/at/campus/ads/service/SessionServiceTest.java
+++ src/test/java/at/campus/ads/service/SessionServiceTest.java
@@ -1,7 +1,6 @@
 package at.campus.ads.service;
 
 import at.campus.ads.persistence.domain.User;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
